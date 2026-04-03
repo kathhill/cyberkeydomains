39a4d1506a066176d85c499c912322009e0381dd
kathhill <kath.hill@sbcglobal.net>
2026-04-03 12:26:58 -0700

    chore: Update yml file and edit config temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // temp github pages deployment
+    site: 'https://kathhill.github.io',
+    base: '/cyberkeydomains',
+    // end temp github page deployment
   vite: {
     plugins: [tailwindcss()]
   }
