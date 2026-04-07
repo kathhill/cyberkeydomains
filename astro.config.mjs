c8a6d55fcc99c949cf18bd033de6b07f75ccaf2c
kathhill <kath.hill@sbcglobal.net>
2026-04-07 12:10:51 -0700

    chore: Update yml file; add favicon and new logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // temp github pages deployment
-    site: 'https://kathhill.github.io',
-    base: '/cyberkeydomains',
-    // end temp github page deployment
+  site: 'https://cyberkeydomains.com',
   vite: {
     plugins: [tailwindcss()]
   }
