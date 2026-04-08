0a2dd19966bec8d4481a2b2409532506ff3cb41a
kathhill <kath.hill@sbcglobal.net>
2026-04-08 14:24:04 -0700

    feat: Add initial content and styles to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -8,5 +8,29 @@ export default defineConfig({
   site: 'https://cyberkeydomains.com',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Montserrat",
+    cssVariable: "--font-montserrat",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/Montserrat-VariableFont_wght.ttf'],
+        weight: '100 900',
+        style: 'normal',        
+      }]
+    }
+  },
+  {
+    provider: fontProviders.local(),
+    name: "Montserrat Italic",
+    cssVariable: "--font-montserrat-italic",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/Montserrat-Italic-VariableFont_wght.ttf'],
+        weight: '100 900',
+        style: 'italic',        
+      }]
+    }
+  }]
 });
